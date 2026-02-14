98a1beec5c3b34292b235c16c48befa333bec898
Vitalii Khrabchuk <vitalik_av@o2.pl>
2026-02-14 20:06:41 +0100

    linkfix

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,13 +2,25 @@ import Link from 'next/link';
 import css from './Header.module.css';
 
 export default function Header() {
-    return (<header className= {
-            css.header
-        }
-
-        > <Link href="/" aria-label="Home" > NoteHub </Link> <nav aria-label="Main Navigation" > <ul className= {
-            css.navigation
-        }
-
-        > <li> <Link href="/" aria-label="Home" > Home </Link> </li> <li> <Link href="/notes" aria-label="Notes" > Notes </Link> </li> </ul> </nav> </header>);
+  return (
+    <header className={css.header}>
+      <Link href="/" aria-label="Home">
+        NoteHub
+      </Link>
+      <nav aria-label="Main Navigation">
+        <ul className={css.navigation}>
+          <li>
+            <Link href="/" aria-label="Home">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/notes/filter/all" aria-label="Filter">
+              Notes
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
